b1151c2f68ed10cb6ebafd6ac70d6782a81ad34a
Cory Grunkemeyer <corygrunk@gmail.com>
2018-02-02 15:27:15 -0700

    Inital commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Simple Node Server
+
+This is a really simple Node.js server that makes a call to Google's Geocoding api.
+https://developers.google.com/maps/documentation/geocoding/start
+
+## Setup
+
+1. Create a `.env` file. You can reference `.env-sample`.
+2. Run `npm install`
+3. Run `node server.js`
---
+++ server.js
@@ -0,0 +1,42 @@
+// server.js
+
+const http = require('http')
+const dotenv = require('dotenv').config()
+const request = require('request')
+const port = process.env.PORT || 3000
+const googleApiKey = process.env.GOOGLE_API_KEY
+
+console.log(`GAPI KEY = ${googleApiKey}`)
+
+if (googleApiKey === '' || googleApiKey === undefined) {
+    console.log('Need to add Google API key to the .env file.')
+    return
+}
+
+// Example Google API Call
+
+let makeGoogleCall = function () {
+    request({ method: 'GET',
+        uri: 'https://maps.googleapis.com/maps/api/geocode/json?address=Sheridan,Wyoming&key=' + googleApiKey
+    },
+    function (error, response, body) {
+        console.log(body)
+    })
+}
+makeGoogleCall()
+
+// Server stuff
+
+const requestHandler = (request, response) => {
+  console.log(request.url)
+  response.end('Hello from the server.')
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('Something bad happened.', err)
+  }
+  console.log(`Listening on ${port}`)
+})
